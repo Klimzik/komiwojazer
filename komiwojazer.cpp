67e4f65d94abf24d783da3813053698b6903f6e8
Mateusz Klimza <mateusz.klimza@idealogic.pl>
2024-06-17 20:51:06 +0200

    update

--- komiwojazer.cpp
+++ komiwojazer.cpp
@@ -5,121 +5,135 @@
 #include <random>
 #include <numeric>
 #include <limits>
+#include <fstream>
 
-const int nodeN = 40;
-std::vector<std::pair<int, int>> nodes(nodeN);
-std::vector<int> saPath;
-std::vector<double> saDataDist;
-double saBestDist;
-std::vector<int> saBestPath;
-double saTempStart = 10000;
-double saTempAlpha = 0.976;
-double saTemp;
+// Constants
+const int NODE_COUNT = 40;
+const int ITERATIONS = 400;
+const double INITIAL_TEMPERATURE = 10000.0;
+const double TEMPERATURE_DECREASE_RATE = 0.976;
 
-int loopN = 400;
+// Class declarations
+class Node {
+public:
+    int x, y;
+    Node(int x, int y) : x(x), y(y) {}
+};
 
-// Function declarations
-std::vector<int> RandPath();
-double PathDist(const std::vector<int>& path);
-void PathMove(std::vector<int>& path, const std::pair<int, int>& move);
-std::pair<int, int> RandAB();
-void SaStep(std::vector<int>& path, double temp);
-void Run();
-void NodeRand();
+class Path {
+public:
+    std::vector<int> nodes;
+
+    Path(int size) : nodes(size) {
+        std::iota(nodes.begin(), nodes.end(), 0);
+        std::random_device rd;
+        std::mt19937 g(rd());
+        //for (int i = nodes.size() - 1; i > 0; --i) {
+        //    int j = rand() % (i + 1);
+        //    std::swap(nodes[i], nodes[j]);
+        //}
+        std::shuffle(nodes.begin(), nodes.end(), g);
+    }
+
+    void swapNodes(int a, int b) {
+        std::swap(nodes[a], nodes[b]);
+    }
+
+    double calculateDistance(const std::vector<Node>& nodeCoordinates) const {
+        double distance = 0.0;
+        for (size_t i = 0; i < nodes.size(); i++) {
+            auto& n0 = nodeCoordinates[nodes[i]];
+            auto& n1 = nodeCoordinates[nodes[(i + 1) % nodes.size()]];
+            distance += std::sqrt((n0.x - n1.x) * (n0.x - n1.x) + (n0.y - n1.y) * (n0.y - n1.y));
+        }
+        return distance;
+    }
+
+    void reverseSubPath(int a, int b) {
+        while (a < b) {
+            swapNodes(a, b);
+            a++;
+            b--;
+        }
+    }
+};
+
+class SimulatedAnnealing {
+public:
+    std::vector<Node> nodeCoordinates;
+    Path bestPath;
+    double bestDistance;
+    double temperature;
+    double temperatureDecreaseRate;
+
+    std::vector<double> distances;
+
+
+    SimulatedAnnealing(int nodeCount, double initialTemperature, double temperatureDecreaseRate)
+        : bestPath(nodeCount), temperature(initialTemperature), temperatureDecreaseRate(temperatureDecreaseRate) {
+        for (int i = 0; i < nodeCount; i++) {
+            nodeCoordinates.emplace_back(rand() % 400, rand() % 200);
+        }
+        bestDistance = bestPath.calculateDistance(nodeCoordinates);
+        //distances.push_back(bestDistance);
+
+    }
+
+    void run(int iterations) {
+        Path currentPath = bestPath;
+        double currentDistance = bestDistance;
+
+        for (int i = 0; i < iterations; ++i) {
+            for (int j = 0; j < 100; ++j) {
+                auto move = getRandomSwapIndices();
+                currentPath.reverseSubPath(move.first, move.second);
+                double newDistance = currentPath.calculateDistance(nodeCoordinates);
+
+                if (newDistance < currentDistance || acceptMove(newDistance, currentDistance)) {
+                    currentDistance = newDistance;
+                    if (newDistance < bestDistance) {
+                        bestDistance = newDistance;
+                        distances.push_back(bestDistance);
+                        bestPath = currentPath;
+                    }
+                }
+                else {
+                    currentPath.reverseSubPath(move.first, move.second);
+                }
+            }
+            temperature *= temperatureDecreaseRate;
+        }
+    }
+    void saveDistancesToFile(const std::string& filename) const {
+        std::ofstream file(filename);
+        for (size_t i = 0; i < distances.size(); ++i) {
+            file << i << "," << distances[i] << "\n";
+        }
+        file.close();
+    }
+
+    void printBestDistance() const {
+        std::cout << "Best path distance: " << bestDistance << std::endl;
+    }
+
+private:
+    std::pair<int, int> getRandomSwapIndices() const {
+        int a = rand() % NODE_COUNT;
+        int b = rand() % (NODE_COUNT - 1);
+        if (b >= a) b++;
+        return (a > b) ? std::make_pair(b, a) : std::make_pair(a, b);
+    }
+
+    bool acceptMove(double newDistance, double currentDistance) const {
+        double probability = std::exp((currentDistance - newDistance) / temperature);
+        return static_cast<double>(rand()) / RAND_MAX < probability;
+    }
+};
 
 int main() {
-    NodeRand();
-    Run();
-    std::cout << "Best path distance: " << saBestDist << std::endl;
+    SimulatedAnnealing sa(NODE_COUNT, INITIAL_TEMPERATURE, TEMPERATURE_DECREASE_RATE);
+    sa.run(ITERATIONS);
+    sa.printBestDistance();
+    sa.saveDistancesToFile("distances.csv");
     return 0;
-}
-
-void Run() {
-    auto startPath = RandPath();
-    double startDist = PathDist(startPath);
-    saPath = startPath;
-    saDataDist.clear();
-    saDataDist.push_back(startDist);
-    saBestDist = startDist;
-    saBestPath = startPath;
-
-    saTemp = saTempStart;
-
-    for (int i = 0; i < loopN; ++i) {
-        SaStep(saPath, saTemp);
-        saTemp = saTemp * saTempAlpha;
-    }
-    saPath = saBestPath;
-    saDataDist.push_back(saBestDist);
-}
-
-void SaStep(std::vector<int>& path, double temp) {
-    double c0 = PathDist(path);
-    double praw = 0, akce = 0;
-    for (int i = 0; i < 100; i++) {
-        auto move = RandAB();
-        PathMove(path, move);
-        double c1 = PathDist(path);
-        if (c1 > c0) {
-            praw = std::exp((c0 - c1) / temp);
-            akce = (static_cast<double>(rand()) / RAND_MAX < praw);
-        }
-        if (c1 <= c0 || akce) {
-            c0 = c1;
-        }
-        else {
-            PathMove(path, move);
-        }
-        if (saBestDist > c0) {
-            saBestDist = c0;
-            saBestPath = path;
-        }
-    }
-    saDataDist.push_back(c0);
-}
-
-std::pair<int, int> RandAB() {
-    int a = rand() % nodeN;
-    int b = rand() % (nodeN - 1);
-    if (b >= a) b++;
-    if (a > b) std::swap(a, b);
-    return { a, b };
-}
-
-void PathMove(std::vector<int>& path, const std::pair<int, int>& move) {
-    int a = move.first, b = move.second;
-    while (a < b) {
-        std::swap(path[a], path[b]);
-        a++;
-        b--;
-    }
-}
-
-double PathDist(const std::vector<int>& path) {
-    double dist = 0;
-    for (size_t i = 0; i < path.size(); i++) {
-        auto& n0 = nodes[path[i]];
-        auto& n1 = nodes[path[(i + 1) % path.size()]];
-        dist += std::sqrt((n0.first - n1.first) * (n0.first - n1.first) + (n0.second - n1.second) * (n0.second - n1.second));
-    }
-    return dist;
-}
-
-void NodeRand() {
-    nodes.clear();
-    saPath.clear();
-    saBestPath.clear();
-    saDataDist.clear();
-    for (int i = 0; i < nodeN; i++) {
-        nodes.push_back({ rand() % 400, rand() % 200 });
-    }
-}
-
-std::vector<int> RandPath() {
-    std::vector<int> path(nodeN);
-    std::iota(path.begin(), path.end(), 0);
-    std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(path.begin(), path.end(), g);
-    return path;
 }
